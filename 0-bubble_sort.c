f6efdc5b793e372e53db816a9faeff2503eb3706
evans-manyala <evans.manyala@gmail.com>
2024-01-22 12:39:35 +0300

    Adding script changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,44 @@
 #include "sort.h"
 /**
  * bubble_sort - sorts an array of integers in ascending order
- * @array: the array to be sorted
- * @size: size of the array
+ * using Bubble Sort Algorithm
+ * @array:  array to be sorted
+ * @size: array size
  */
+
+void swapped(int *num1, int *num2)
+{
+	int temp;
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
 
 void bubble_sort(int *array, size_t size)
 {
 	size_t x, y;
-	int temp, swapped;
+	int num_of_swaps;
 
 	if (array == NULL || size < 2) /* Check for NULL array or size less than 2 */
 		return;
 
 	for (x = 0; x < size - 1; x++)
 	{
-		swapped = 0; /* Flag to track if any swaps occurred in a pass */
-
+		num_of_swaps = 0;
 		/* Iterate through adjacent elements in each pass */
 		for (y = 0; y < size - x - 1; y++)
 		{
 			/* If elements are out of order, swap them */
 			if (array[y] > array[y + 1])
 			{
-				temp = array[y];
-				array[y] = array[y + 1];
-				array[y + 1] = temp;
-				swapped = 1;
+				num_of_swaps = 1;
+				swapped(&array[y], &array[y + 1]);
 				/* Print the array after the swap */
 				print_array(array, size);
 			}
 		}
 		/* If no swaps occurred in a pass, the array is sorted, so break */
-		if (swapped == 0)
-			return;
+		if (num_of_swaps == 0)
+			break;
 	}
 }
