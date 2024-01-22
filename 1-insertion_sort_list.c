866633c3321c025e0baf68a4c2e1aae4f5b56239
evans-manyala <evans.manyala@gmail.com>
2024-01-22 13:12:03 +0300

    Adding files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,26 @@
 #include "sort.h"
+/**
+ * insertion_sort_list - Function performs insertions sort
+ * on a list.
+ * @list: List to be sorted using insertion sort.
+*/
 
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *next;
-	
+
 	if (!*list || !(*list)->next)
 		return; /*Empty list or single element*/
+	for (current = *list; current; current = current->next)
+	{
+		prev = NULL;
+		next = current->next;
+
+		/*Find the insertion point for current node*/
+		for (prev = current->prev; prev; prev = prev->prev)
+		{
+			if (prev->n > current->n)
+				break;
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -18,7 +18,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/*SORTING ALGORITHMS*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+/*HELPER FUNCTIONS*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
