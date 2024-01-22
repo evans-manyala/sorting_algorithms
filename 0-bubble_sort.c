333ddc8985abfb2624459506521e62680b3b372c
evans-manyala <evans.manyala@gmail.com>
2024-01-22 12:46:47 +0300

    Adding Code Syntax fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,24 @@
 #include "sort.h"
 /**
+ * swapped - Function swaps values in array.
+ * @num1: 1st value in array to be swapped.
+ * @num2: 2nd value in array to be swapped.
+*/
+void swapped(int *num1, int *num2)
+{
+	int temp;
+
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+/**
  * bubble_sort - sorts an array of integers in ascending order
  * using Bubble Sort Algorithm
  * @array:  array to be sorted
  * @size: array size
  */
-
-void swapped(int *num1, int *num2)
-{
-	int temp;
-	temp = *num1;
-	*num1 = *num2;
-	*num2 = temp;
-}
 
 void bubble_sort(int *array, size_t size)
 {
