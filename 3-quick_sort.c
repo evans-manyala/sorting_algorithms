3976151ff74fb91c7f17f2db89c85bba5f6b16b1
Evans Manyala <evans.manyala@gmail.com>
2024-01-23 08:02:29 +0300

    Adding Lomuto quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -43,3 +43,20 @@ size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
 
 	return (x + 1);
 }
+/**
+ * lomuto_quick_sort - Recursive function to perform Lomuto Quick Sort.
+ * @array: array to be sorted
+ * @low: low index of the partition
+ * @high: high index of the partition
+ * @size: array size
+ */
+void lomuto_quick_sort(int *array, ssize_t low, ssize_t high, size_t size)
+{
+	if (low < high)
+	{
+		size_t pivot_index = lomuto_partition(array, low, high, size);
+
+		lomuto_quick_sort(array, low, pivot_index - 1, size);
+		lomuto_quick_sort(array, pivot_index + 1, high, size);
+	}
+}
