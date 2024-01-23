0701c8a0fdb24dd328f95a14cac962aa1a53c098
Evans Manyala <evans.manyala@gmail.com>
2024-01-23 14:55:56 +0300

    Adding script chnages

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,59 +7,54 @@
  */
 void swapped(int *num1, int *num2)
 {
-	int temp;
-
-	temp = *num1;
+	int temp = *num1;
 	*num1 = *num2;
 	*num2 = temp;
 }
-
 /**
  * lomuto_partition - Lomuto partition scheme for Quick Sort.
  * @array: array to be partitioned
  * @low: low index of the partition
  * @high: high index of the partition
- * @size: array size
  * Return: index of the pivot
  */
-size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
+size_t lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	ssize_t x = low - 1;
-	ssize_t y;
+	int x = low - 1;
+	int y;
 
-	for (y = low; y <= high - 1; y++)
+	for (y = low; y < high; y++)
 	{
-		if (array[y] < pivot)
+		if (array[y] <= pivot)
 		{
 			x++;
 			swapped(&array[x], &array[y]);
-			print_array(array, size);
+			print_array(array, high + 1);
 		}
 	}
 
 	swapped(&array[x + 1], &array[high]);
-	print_array(array, size);
-
+	print_array(array, high + 1);
 	return (x + 1);
 }
 /**
- * lomuto_quick_sort - Recursive function to perform Lomuto Quick Sort.
+ * quick_sort_helper- Recursive function to perform Lomuto Quick Sort.
  * @array: array to be sorted
  * @low: low index of the partition
  * @high: high index of the partition
- * @size: array size
  */
-void lomuto_quick_sort(int *array, ssize_t low, ssize_t high, size_t size)
+void quick_sort_helper(int *array, int low, int high)
 {
 	if (low < high)
 	{
-		size_t pivot_index = lomuto_partition(array, low, high, size);
+		int pivot_index = lomuto_partition(array, low, high);
 
-		lomuto_quick_sort(array, low, pivot_index - 1, size);
-		lomuto_quick_sort(array, pivot_index + 1, high, size);
+		quick_sort_helper(array, low, pivot_index - 1);
+		quick_sort_helper(array, pivot_index + 1, high);
 	}
 }
+
 /**
  * quick_sort - sorts an array of integers in ascending order
  * using Quick Sort Algorithm (Lomuto partition scheme)
@@ -68,8 +63,10 @@ void lomuto_quick_sort(int *array, ssize_t low, ssize_t high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (array == NULL || size <= 1)
+	{
 		return;
+	}
 
-	lomuto_quick_sort(array, 0, size - 1, size);
+	quick_sort_helper(array, 0, size - 1);
 }
