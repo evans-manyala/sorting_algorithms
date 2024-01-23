c11590892e1b7d36c63f2d7ae766d29d3f40c585
Evans Manyala <evans.manyala@gmail.com>
2024-01-23 23:46:41 +0300

    Reverting back tp previous code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,51 +7,57 @@
  */
 void swapped(int *num1, int *num2)
 {
-	int temp = *num1;
+	int temp;
+
+	temp = *num1;
 	*num1 = *num2;
 	*num2 = temp;
 }
+
 /**
  * lomuto_partition - Lomuto partition scheme for Quick Sort.
  * @array: array to be partitioned
  * @low: low index of the partition
  * @high: high index of the partition
+ * @size: array size
  * Return: index of the pivot
  */
-size_t lomuto_partition(int *array, int low, int high)
+size_t lomuto_partition(int *array, size_t low, size_t high, size_t size)
 {
 	int pivot = array[high];
-	int x = low - 1;
-	int y;
+	size_t x = low - 1;
+	size_t y;
 
-	for (y = low; y < high; y++)
+	for (y = low; y <= high - 1; y++)
 	{
-		if (array[y] <= pivot)
+		if (array[y] < pivot)
 		{
 			x++;
 			swapped(&array[x], &array[y]);
-			print_array(array, high + 1);
+			print_array(array, size);
 		}
 	}
 
 	swapped(&array[x + 1], &array[high]);
-	print_array(array, high + 1);
+	print_array(array, size);
+
 	return (x + 1);
 }
 /**
- * quick_sort_helper- Recursive function to perform Lomuto Quick Sort.
+ * quick_sort_helper - Recursive function to perform Lomuto Quick Sort.
  * @array: array to be sorted
  * @low: low index of the partition
  * @high: high index of the partition
+ * @size: array size
  */
-void quick_sort_helper(int *array, int low, int high)
+void quick_sort_helper(int *array, ssize_t low, ssize_t high, size_t size)
 {
 	if (low < high)
 	{
-		int pivot_index = lomuto_partition(array, low, high);
+		size_t pivot_index = lomuto_partition(array, low, high, size);
 
-		quick_sort_helper(array, low, pivot_index - 1);
-		quick_sort_helper(array, pivot_index + 1, high);
+		quick_sort_helper(array, low, pivot_index - 1, size);
+		quick_sort_helper(array, pivot_index + 1, high, size);
 	}
 }
 
@@ -63,10 +69,8 @@ void quick_sort_helper(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1)
-	{
+	if (array == NULL || size < 2)
 		return;
-	}
 
-	quick_sort_helper(array, 0, size - 1);
+	quick_sort_helper(array, 0, size - 1, size);
 }
