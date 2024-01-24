482582c04584dc2d03058614d600a8d5edb3f9f1
Evans Manyala <evans.manyala@gmail.com>
2024-01-24 22:53:36 +0300

    Initial script commit

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - Sorts a doubly linked list of integers
+ *                      in ascending order using Cocktail Shaker Sort.
+ * @list: A pointer to the head of the doubly linked list.
+ */
+void cocktail_sort_list(listint_t **list);
+{
+}
