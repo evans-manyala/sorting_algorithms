ebeac5326b8bc830cde326f1ba73387b1d67f67d
Evans Manyala <evans.manyala@gmail.com>
2024-01-24 15:28:55 +0300

    Adding initial script

---
+++ 100-shell_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ *              using Shell Sort Algorithm with Knuth sequence.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void shell_sort(int *array, size_t size)
+{
+}
